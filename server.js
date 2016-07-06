4b4489d767b540c2398ffc8ca3b7dd011ecd1960
Matt Higbee <mhigbee@hotmail.com>
2016-07-05 20:54:26 -0600

    saving progress for baseline RESTFUL API

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var mongoJS = require('mongojs');
 var app = express();
 
 // the first argument is the connection string and the second arg is the collections
-var db = mongoJS('devblog', ['post']);
+var db = mongoJS('devblog', ['posts']);
 var port = 3000;
 
+var Posts = db.getCollection('posts');
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -17,12 +18,47 @@ app.use(cors());
 
 // post
 
+app.post('/api/posts', function (req,res){
+   db.posts.save(req.body, function(err, response){
+       if(err) return res.status(500).json(err);
+       else return res.json(response);
+   });
+});
+
 
 // get
+app.get('/api/posts', function(req,res){
+    var query = {};
+    if(req.query.name) query.name = new RegExp(req.query.name);
+    if(req.query.date) query.date = req.query.date;
+    if(req.query.id) query._id = mongoJS.ObjectID(req.query.id);
+   db.posts.find(query, function(err,result){
+       if(err) return res.status(500).json(err);
+       else return res.json(result);
+   });
+
+});
+
 
 // put
 
+app.put('/api/posts',function(req,res) {
+    db.posts.findAndModify({query: {_id: mongoJS.ObjectId(req.query.id)}, update: {$set: req.body },new: true},
+        function(err, response){
+            if(err) res.status(500).json(err);
+            else res.json(response);
+        })
+});
+
+
 // delete
+
+app.delete('/api/posts', function(req,res){
+    db.posts.remove( {_id: mongoJS.ObjectId(req.query.id)}, function(err, response){
+        if(err) res.status(500).json(err);
+        else return res.json(response);
+    })
+});
 
 
 app.listen(port, function(){
